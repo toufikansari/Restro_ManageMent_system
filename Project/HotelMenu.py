f992316c74b9d0248a4c6c436fc10a1d39b4df3d
toufikansari <toufikmdansari@gmail.com>
2026-03-27 10:10:40 +0530

    Fully Done

--- Project/HotelMenu.py
+++ Project/HotelMenu.py
@@ -30,3 +30,25 @@ for item,price in menu.items():
 
 
 # Order Calculate 
+Order_total = 0
+
+Item_1 = input("Enter the name of item you want to order :")
+if Item_1 in menu:
+    Order_total += menu[Item_1] # 0 + 50
+    print(f"Your item {Item_1} has been added your order")
+else:
+    (f"Order item : {Item_1} is not avaible yet")
+
+
+# Added More item :
+another_item = input("Doy you Want to Add another item?(Yes / No) ")
+
+if another_item == "Yes":
+    item_two = input("Enter the name of second item = ")
+    if item_two in menu:
+        Order_total += menu[item_two]
+        print(f"Order item {item_two} has benn added your order :")
+    else:
+        (f"Order item : {item_two} is not avaible yet")
+
+print(f"The total amount of item to you pay is :{Order_total} ")
