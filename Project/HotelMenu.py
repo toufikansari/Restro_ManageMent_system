58c50b61f9435e23496623e233f4b50ffbc0b15c
toufikansari <toufikmdansari@gmail.com>
2026-03-27 09:20:47 +0530

    Creat Menu Card

---
+++ Project/HotelMenu.py
@@ -0,0 +1,24 @@
+# Menu Card usnig Disco
+
+menu = {
+    'Pizza': 40,
+    'Pasta': 50,
+    'Burger': 60,
+    'Salad': 70,
+    'Coffee': 80,
+    'Tea': 20,
+    'Sandwich': 45,
+    'Fries': 35,
+    'Noodles': 55,
+    'Momos': 50,
+    'Ice Cream': 30,
+    'Cake': 90,
+    'Juice': 60,
+    'Shake': 70,
+    'Biryani': 120,
+    'Fried Rice': 100,
+    'Paneer Tikka': 150,
+    'Chicken Curry': 180,
+    'Dal Makhani': 110,
+    'Roti': 10
+}
